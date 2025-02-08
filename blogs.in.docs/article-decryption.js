c42db5eb5621afc651d751649944f36b1ea4716e
liumingedwin <liumingedwin@163.com>
2025-02-08 16:22:56 +0800

    Upd: encryption and tidy v1

---
+++ blogs.in.docs/article-decryption.js
@@ -0,0 +1,15 @@
+window.$docsify.plugins = window.$docsify.plugins || [];
+(function decrypt_docsify_content_plug() {
+    return window.decryption_plug = (hook, vm) => {
+        hook.beforeEach(function (content) {
+            let scontent = content.toString()
+            console.log(scontent, typeof scontent)
+            const defprefix = "aes";
+            if (scontent.indexOf(defprefix) != 0)
+                return scontent;
+            return CryptoJS.AES.decrypt(String.prototype.replace.call(scontent, defprefix, ""), prompt("Input: ", "password")).toString(CryptoJS.enc.Utf8)
+                ;
+        })
+    };
+})();
+window.$docsify.plugins = [window.decryption_plug, ...window.$docsify.plugins];
