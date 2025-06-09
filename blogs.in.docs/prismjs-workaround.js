5f8d972ddb90457f0fb1ee9dc4f49d918a06c932
liumingedwin <liumingedwin@163.com>
2025-06-09 13:32:12 +0800

    feat: some optimizations onload

--- blogs.in.docs/prismjs-workaround.js
+++ blogs.in.docs/prismjs-workaround.js
@@ -14,25 +14,29 @@ console.log("qwq");
 console.log($docsify)
 window.$docsify.plugins = [window.prismjs_workaround, ...window.$docsify.plugins];
 */
-((function() {
+(function () {
     // 选择需要观察变动的节点
-const targetNode = document.querySelector("body");
-console.log(targetNode)
-// 观察器的配置（需要观察什么变动）
-const config = { subtree: true, childList: true, attributes: true };
-let las = new Date()
-// 当观察到变动时执行的回调函数
-const callback = function (mutationsList, observer) {
-    if(new Date() - las <= 6000) return;
-    las = new Date();
-    observer.disconnect();
-    if(Prism)
-    Prism.highlightAll();
+    // const targetNode = document.querySelector("article#main");
+    const targetNode = document.querySelector("body");
+    console.log(targetNode)
+    // 观察器的配置（需要观察什么变动）
+    const config = { subtree: true, childList: true, attributes: true };
+    let las = new Date()
+    // 当观察到变动时执行的回调函数
+    const callback = function (mutationsList, observer) {
+        if (new Date() - las <= 6000) return;
+        las = new Date();
+        observer.disconnect();
+        const loc = document.querySelector("article#main");
+        if (Prism)
+            // Prism.highlightAll();
+            Prism.highlightAllUnder(loc ? loc : targetNode);
+        if(observer)
+        observer.observe(targetNode, config);
+    };
+    const observer = new MutationObserver(callback);
     observer.observe(targetNode, config);
-};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
 
-
-// 之后，可停止观察
-}))();
+    requestAnimationFrame(callback)
+    // 之后，可停止观察
+})();
